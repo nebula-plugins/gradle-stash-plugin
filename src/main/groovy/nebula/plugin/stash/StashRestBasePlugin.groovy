26800bd59a78f82e97782f3001dc472222ce2595
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-11-02 10:47:08 -0700

    Revert "tasks.withType: use configureEach"
    This reverts commit a67f1d9a4c275163405306eb278ada8c74bec3bf.

--- src/main/groovy/nebula/plugin/stash/StashRestBasePlugin.groovy
+++ src/main/groovy/nebula/plugin/stash/StashRestBasePlugin.groovy
@@ -1,7 +1,6 @@
 package nebula.plugin.stash
 
 import nebula.plugin.stash.tasks.StashTask
-import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -15,15 +14,12 @@ class StashRestBasePlugin implements Plugin<Project> {
     }
 
     private void configureStashTasks(Project project, StashPluginExtension extension) {
-        project.tasks.withType(StashTask).configureEach(new Action<StashTask>() {
-            @Override
-            void execute(StashTask stashTask) {
-                stashTask.conventionMapping.stashRepo = { project.hasProperty('stashRepo') ? project.stashRepo : extension.stashRepo }
-                stashTask. conventionMapping.stashProject = { project.hasProperty('stashProject') ? project.stashProject : extension.stashProject }
-                stashTask. conventionMapping.stashHost = { project.hasProperty('stashHost') ? project.stashHost : extension.stashHost }
-                stashTask.conventionMapping.stashUser = { project.hasProperty('stashUser') ? project.stashUser : extension.stashUser }
-                stashTask.conventionMapping.stashPassword = { project.hasProperty('stashPassword') ? project.stashPassword : extension.stashPassword }
-            }
-        })
+        project.tasks.withType(StashTask) {
+            conventionMapping.stashRepo = { project.hasProperty('stashRepo') ? project.stashRepo : extension.stashRepo }
+            conventionMapping.stashProject = { project.hasProperty('stashProject') ? project.stashProject : extension.stashProject }
+            conventionMapping.stashHost = { project.hasProperty('stashHost') ? project.stashHost : extension.stashHost }
+            conventionMapping.stashUser = { project.hasProperty('stashUser') ? project.stashUser : extension.stashUser }
+            conventionMapping.stashPassword = { project.hasProperty('stashPassword') ? project.stashPassword : extension.stashPassword }
+        }
     }
 }
